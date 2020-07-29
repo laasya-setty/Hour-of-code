32489b02101e7ed0095f5ad4a2275db292c2fa03
laasya-setty <45589527+laasya-setty@users.noreply.github.com>
2020-07-29 14:47:41 +0530

    first commit

---
+++ Palindrome.java
@@ -0,0 +1,38 @@
+
+class Palindrome{
+    
+     static int check(int n){
+        int sum=0,rem;
+         if(n!=0){
+             rem=n%10;
+           sum=(sum*10)+rem;
+           n=n/10;
+           check(n);
+           
+         }
+         return (sum==n)?1:0;  //ternary opertaion
+    }
+    static void range(int min,int max){
+        for(int i=min;i<max;i++){
+            if(check(i)==1){
+                System.out.println(i);
+            }
+        }
+    }
+
+    
+
+
+
+
+
+    public static void main(String args[]){
+        range(10,100);
+        
+        
+       
+        
+
+
+    }
+}

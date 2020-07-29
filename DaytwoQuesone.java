6eb9fff70ac81fcbc4348ab9e1d934c46e3b296e
laasya-setty <45589527+laasya-setty@users.noreply.github.com>
2020-07-29 17:08:20 +0530

    Day 2
    question one

---
+++ DaytwoQuesone.java
@@ -0,0 +1,25 @@
+/*Write a program to display the multiples of a number 'n' upto the given limit 'L'
+ and print the sum of all the multiples in it.*/
+import java.util.*;
+ public class DaytwoQuesone {
+    public static void main(String args[]){
+     Scanner sc=new Scanner(System.in);
+     System.out.println("enter the number");
+     int start=sc.nextInt();
+     System.out.println("Enter the limit");
+     int limit=sc.nextInt();
+     sc.close();
+     int multiple=start;
+     int next=0,sum=0;
+     System.out.printf("the multiples of %d are",start);
+     while(limit!=0){
+         next=next+multiple;
+         sum=sum+next;
+         System.out.println(next);
+         limit--;
+     }
+     System.out.println("The total sum of multiples is");
+     System.out.println(sum);
+    }
+    
+}
